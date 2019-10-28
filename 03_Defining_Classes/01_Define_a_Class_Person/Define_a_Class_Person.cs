b6b78ced5348f37ded8618a9db2c537fd1e52f22
hidrawlik <55383070+hidrawlik@users.noreply.github.com>
2019-10-28 10:25:36 +0200

    Lab_3

---
+++ 03_Defining_Classes/01_Define_a_Class_Person/Define_a_Class_Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _01_Define_a_Class_Person
+{
+    class Person
+    {
+        int age;
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+        public void  GetInfo()
+        {
+            Console.WriteLine($"{Name}  {Age}");
+        }
+    }
+    class Define_a_Class_Person
+    {
+        static void Main()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Person[] person = new Person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                person[i] = new Person();
+
+                Console.Write($"\nName[{i}]: ");
+                person[i].Name = Console.ReadLine();
+
+                Console.Write($"Age[{i}]: ");
+                person[i].Age = int.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                person[i].GetInfo();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
